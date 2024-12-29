3eade2d1651fc9a61fd7a849be7bc784cd8533b3
AbheetHacker4278 <114290748+AbheetHacker4278@users.noreply.github.com>
2024-12-29 22:57:49 +0530

    new updates

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -61,7 +61,7 @@ export const Home: React.FC = () => {
   if (!assets || !Array.isArray(assets)) {
     return (
       <div className="bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
-        Invalid data format received from the API.
+        Invalid data format received from the Api.
       </div>
     );
   }
